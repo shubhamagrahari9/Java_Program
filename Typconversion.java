2b773746dfbb998842424671dbc0f2887e94ec47
Shubham Agrahari <Shubham.hari78@gmail.com>
2025-01-04 01:29:32 +0530

    New

--- Typconversion.java
+++ Typconversion.java
@@ -1,5 +1,14 @@
+import java.util.*;
 public class Typconversion {
-    
+    public static void main(String args[]){
+        System.out.println("Enter a number:");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println("Now we will convert it into float data type:-");
+        float y = x;
+        System.out.println(y);
+    }
+    // This is called implicit or widening type conversion.
 }
 /*1. Type Conversion occurs when we convert a data type into another data type.
   2. For Conversion, data type should be compatible with another data type e.g. we can
