d4fe3773796666a3448309e21594d1612afc76ec
Shubham Agrahari <Shubham.hari78@gmail.com>
2025-01-05 00:24:23 +0530

    new

---
+++ Typecast.java
@@ -0,0 +1,12 @@
+public class Typecast {
+    public static void main(String args[]){
+        float x= 10.899f;
+        int y = (int)x;
+        System.out.println(y);
+    }
+    
+}
+/*
+  TypeCasting/Explicit/Narrowing Conversion:-
+
+ */
