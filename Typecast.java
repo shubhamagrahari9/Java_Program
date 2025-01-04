a5b56e2605ec88254ea6c7279f24ffe73f782390
Shubham Agrahari <Shubham.hari78@gmail.com>
2025-01-05 00:34:00 +0530

    "new"

--- Typecast.java
+++ Typecast.java
@@ -1,7 +1,7 @@
 public class Typecast {
     public static void main(String args[]){
         float x= 10.899f;
-        int y = (int)x;
+        int y = (int) x;
         System.out.println(y);
     }
     
